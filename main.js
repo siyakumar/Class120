696021246a6eea960384c12a57dfbdef96b432b6
siyakumar <85111217+siyakumar@users.noreply.github.com>
2021-11-27 12:38:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function modelLoaded()
+{
+ console.log('modelLoaded');
+}
+
+function draw()
+{
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+  
+}
+
+var previousResult=''
+
+function gotResult(error, results)
+{
+  if (error) {
+    console.error(error);
+  } else {
+    if ((results[0].confidence>0.5)&&(previousResult != results[0].label)) {
+      console.log(results);
+      previousResult=results[0].label;
+      var synth=window.speechsynthesis;
+      speakData='object detected is '+result[0].label;
+      var utterThis=new SpeechSynthesisUtterance(speakData);
+      synth.speak(utterThis);
+      document.getElementById("result_object_name").innerHTML=results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+  }
+}
